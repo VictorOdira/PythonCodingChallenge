f325be99642ca20d5c83ca7c75439004aabf08fe
VictorOdira <118199038+VictorOdira@users.noreply.github.com>
2023-05-07 16:45:09 +0300

    Add files via upload

--- Day2Project-TipCalculator/main.py
+++ Day2Project-TipCalculator/main.py
@@ -1,1 +1,12 @@
+print("Tip Calculator!")
+bill = float(input("Enter the Total Bill: "))
+percent_tip = float(input("Enter Percentage of Bill as Tip: "))
+num_people = int(input("Enter Number of Friends to Share the Bill: "))
+
+tip = bill * (percent_tip/100)
+bill_with_tip = bill + tip
+bill_per_person = round(bill_with_tip/num_people, 2)
+
+print(f"Total Bill is Kes {bill_with_tip} and Tip is Kes {tip} and Bill per Head is Kes {bill_per_person}")
+
 
